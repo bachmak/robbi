1e450fd67b3846fa15976bee037bd8999becd2d4
Dima Kochetov <dima.kochetov.70@gmail.com>
2025-11-29 15:34:10 +0000

    Move wheels based on subscription

--- src/app/lab_6.cpp
+++ src/app/lab_6.cpp
@@ -4,12 +4,12 @@
 #include "ros_oop/node.h"
 #include "ros_oop/subscription.h"
 #include "ros_oop/executor.h"
+#include "ros_oop/publisher.h"
 #include "ros_oop/timer.h"
 #include "io_utils.h"
 #include "time_utils.h"
+#include "robot_utils.h"
 #include "utils/connection.h"
-
-#include <Arduino.h>
 
 namespace lab_6
 {
@@ -21,23 +21,37 @@ namespace lab_6
             .delay_after_init = Ms{2000},
         };
 
-        const char *node_name = "micro_ros_platformio_node";
-        const char *topic_name = "micro_ros_platformio_node_publisher";
+        RobotSettings robot_settings = {
+            .left_wheel_settings = {
+                .control_pin = Pin{5},
+                .feedback_pin = Pin{6},
+                .speed_dead_range = Speed{4},
+                .circumference = Meter{0.205},
+                .label = "right-wheel",
+            },
+            .right_wheel_settings = {
+                .control_pin = Pin{7},
+                .feedback_pin = Pin{8},
+                .speed_dead_range = Speed{4},
+                .circumference = Meter{0.205},
+                .label = "left-wheel",
+            },
+            .width = Meter{0.102},
+        };
+
+        const char *node_name = "wheels";
+        const char *topic_name = "move";
 
         Ms ping_interval{500};
         Ms ping_timeout{100};
 
-        Ms timer_period{1000};
-        Ms loop_delay{100};
         Ms spin_timeout{100};
         Ms connection_check_period{500};
-
-        Pin blink_pin{13};
     };
 
     static const auto config = Config{};
 
-    void do_loop(const Config &config)
+    void do_loop(Wheels &wheels, const Config &config)
     {
         auto ping_timer = time_utils::Timer{
             config.ping_interval,
@@ -50,14 +64,41 @@ namespace lab_6
 
         auto support = ros::Support{};
         auto node = ros::Node{support, config.node_name};
-        auto subscription_callback = [&config](int32_t data)
+
+        auto wa_left = wheels.left.attach();
+        auto wa_right = wheels.right.attach();
+
+        auto motors_move = [&](Speed left, Speed right)
         {
-            digitalWrite(config.blink_pin.v, data);
+            wheels.left.rotate(wa_left, left);
+            wheels.right.rotate(wa_right, right);
+        };
+
+        auto subscription_callback = [&](int32_t data)
+        {
+            switch (data)
+            {
+            case 0:
+                motors_move(Speed{0}, Speed{0});
+                return;
+            case 1:
+                motors_move(Speed{1}, Speed{-1});
+                return;
+            case 2:
+                motors_move(Speed{-1}, Speed{1});
+                return;
+            case 3:
+                motors_move(Speed{-1}, Speed{-1});
+                return;
+            case 4:
+                motors_move(Speed{1}, Speed{1});
+                return;
+            }
         };
 
         auto subscription = ros::Subscription<int32_t>{
             node,
-            "micro_ros_arduino_subscriber",
+            config.topic_name,
             subscription_callback};
 
         auto executables = std::vector<ros::Executable>{
@@ -68,7 +109,6 @@ namespace lab_6
 
         while (!utils::connection::is_disconnected(ping_timer, config.ping_timeout))
         {
-            delay(config.loop_delay.count());
             executor.spin_some(config.spin_timeout);
         }
     }
@@ -76,14 +116,16 @@ namespace lab_6
     void run()
     {
         const auto config = Config{};
+        auto wheels = Wheels{
+            config.robot_settings.left_wheel_settings,
+            config.robot_settings.right_wheel_settings,
+        };
 
         io_utils::init(config.io_setings);
 
-        pinMode(config.blink_pin.v, OUTPUT);
-
         while (true)
         {
-            do_loop(config);
+            do_loop(wheels, config);
         }
     }
 }
