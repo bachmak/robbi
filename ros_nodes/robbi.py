c45cf1af61a41e8d0d2308b04ca1a7168f6b97c1
Dima Kochetov <dima.kochetov.70@gmail.com>
2025-12-15 09:38:29 +0000

    Update wall follower

--- ros_nodes/robbi.py
+++ ros_nodes/robbi.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import rclpy
-from rclpy.node import Node
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Range
 import math
 import time
 
+from geometry_msgs.msg import Twist
+from rclpy.node import Node
+from sensor_msgs.msg import Range
+from std_msgs.msg import String
 
 class Robbi(Node):
     def __init__(self):
@@ -15,42 +16,42 @@ class Robbi(Node):
         # ---- Parameters ----
         self.declare_parameter('range_topic', '/range')
         self.declare_parameter('cmd_vel_topic', '/cmd_vel')
+        self.declare_parameter('cmd_action_topic', '/cmd_action')
         self.declare_parameter('wall_side', 'left')
-        self.declare_parameter('desired_distance_m', 0.20)
-        self.declare_parameter('distance_tolerance_m', 0.02)
+        self.declare_parameter('desired_distance_m', 0.2)
         self.declare_parameter('front_obstacle_m', 0.40)
         self.declare_parameter('forward_speed', 0.1)
-        self.declare_parameter('kp_angular', 1.0)
-        self.declare_parameter('control_rate', 2.0)
-        self.declare_parameter('turn_angular_speed', 1.5)
-        self.declare_parameter('turn_safe_margin_s', 0.5)
+        self.declare_parameter('kp_angular', 2.5)
+        self.declare_parameter('control_rate', 20.0)
+        self.declare_parameter('turn_angular_speed', 2.0)
 
         # ---- Load parameters ----
         range_topic = self.get_parameter('range_topic').value
         cmd_vel_topic = self.get_parameter('cmd_vel_topic').value
+        cmd_action_topic = self.get_parameter('cmd_action_topic').value
         self.wall_side = self.get_parameter('wall_side').value
         self.desired_distance = self.get_parameter('desired_distance_m').value
-        self.dist_tolerance = self.get_parameter('distance_tolerance_m').value
         self.front_obstacle = self.get_parameter('front_obstacle_m').value
         self.forward_speed = self.get_parameter('forward_speed').value
         self.kp = self.get_parameter('kp_angular').value
         rate = self.get_parameter('control_rate').value
         self.turn_speed = self.get_parameter('turn_angular_speed').value
-        self.turn_margin = self.get_parameter('turn_safe_margin_s').value
 
         # Sensor storage
         self.ranges = { "angle_0": None, "angle_90": None, "angle_180": None }
 
         self.create_subscription(Range, range_topic, self.range_callback, 10)
-        self.cmd_pub = self.create_publisher(Twist, cmd_vel_topic, 10)
+        self.twist_pub = self.create_publisher(Twist, cmd_vel_topic, 10)
+        self.action_pub = self.create_publisher(String, cmd_action_topic, 10)
 
-        self.state = "SEARCH"    # <<< NEW >>>
-        self.turn_duration = (2 * math.pi) / self.turn_speed
+        self.state = "SEARCH"
+        self.turn_duration = 4
+        self.turn_angle = 180
+
+        self.turn_start_time = 0
 
         self.min_left = float('inf')
         self.min_right = float('inf')
-
-        self.rotation_is_done = False
 
         self.create_timer(1.0 / rate, self.update)
         self.get_logger().info("Robbi: SEARCHING for wall first.")
@@ -58,6 +59,9 @@ class Robbi(Node):
 
     # ---------------------------- RANGE CALLBACK ----------------------------
     def range_callback(self, msg: Range):
+        if self.state == "TURN":
+            return
+
         fid = msg.header.frame_id
         if fid in self.ranges:
             self.ranges[fid] = msg.range / 100.0  # cm → m
@@ -72,8 +76,6 @@ class Robbi(Node):
         left = self.ranges["angle_0"]
         front = self.ranges["angle_90"]
         right = self.ranges["angle_180"]
-
-        twist = Twist()
 
         # ===============================================================
         # STATE: SEARCH
@@ -95,20 +97,37 @@ class Robbi(Node):
             self.state = "FOLLOW"
             return
 
+        if self.state == "TURN":
+            if time.time() - self.turn_start_time < self.turn_duration:
+                return
+
+            if self.wall_side == "left":
+                self.wall_side = "right"
+            else:
+                self.wall_side = "left"
+
+            self.ranges["angle_0"], self.ranges["angle_180"] = \
+                self.ranges["angle_180"], self.ranges["angle_0"]
+
+            self.ranges["angle_90"] = None
+            self.state = "FOLLOW"
+            return
+
+
         if self.state == "FOLLOW":
             if 0 < front < self.front_obstacle:
-                if not self.rotation_is_done:
-                    twist.angular.z = self.turn_speed
-                    self.cmd_pub.publish(twist)
-                    time.sleep(self.turn_duration / 2)
-                    self.rotation_is_done = True
+                self.twist_pub.publish(Twist())
+                time.sleep(1)
 
-                self.cmd_pub.publish(Twist())
-                if self.wall_side == "left":
-                    self.wall_side = "right"
-                else:
-                    self.wall_side = "left"
+                action = String()
+                action.data = f"rotate {self.turn_angle} {self.turn_duration}"
+                self.action_pub.publish(action)
 
+                self.turn_start_time = time.time()
+                self.state = "TURN"
+                return
+
+                time.sleep(self.turn_duration + 0.5)
                 return
 
         # Select wall side
@@ -124,11 +143,15 @@ class Robbi(Node):
         if angular < 0.0:
             angular *= 1.5
 
-        self.get_logger().info(f"angular={angular}")
+        self.get_logger().info(f"error={error}, angular={angular}, wall_side={self.wall_side} side={side}")
 
+        twist = Twist()
         twist.linear.x = self.forward_speed
         twist.angular.z = angular
-        self.cmd_pub.publish(twist)
+        self.twist_pub.publish(twist)
+
+    def destroy(self):
+        super().destroy_node()
 
 
 def main(args=None):
@@ -139,7 +162,7 @@ def main(args=None):
     except KeyboardInterrupt:
         pass
     finally:
-        node.cmd_pub.publish(Twist())
+        node.twist_pub.publish(Twist())
         node.destroy_node()
         rclpy.shutdown()
 
