bf563327a06bd4e82b3814be59d2601a8ba63a64
Dima Kochetov <dima.kochetov.70@gmail.com>
2025-12-15 15:44:49 +0000

    Add IMU data reading to main for nano

--- src/nano33ble/main.cpp
+++ src/nano33ble/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <Arduino_LSM9DS1.h>
+#include <MadgwickAHRS.h>
 #include <Servo.h>
 
 #include <array>
@@ -36,12 +38,13 @@ struct USContext
     };
 
     Servo servo;
-    uint32_t last_update_time_ms = 0;
     int last_position_idx = 0;
 
     const int servo_pin = 12;
     const int trig_pin = 8;
     const int echo_pin = 9;
+
+    uint32_t last_update_time_ms = 0;
     const uint32_t update_interval_ms = 600;
 
     const std::array<MeasurementPosition, 4> measurement_positions = {
@@ -55,6 +58,28 @@ struct USContext
 
 struct IMUContext
 {
+    Madgwick filter;
+
+    const float offset_gx = 1.83;
+    const float offset_gy = -0.75;
+    const float offset_gz = -0.30;
+
+    const float offset_ax = 0.005;
+    const float offset_ay = -0.018;
+    const float offset_az = 0.018;
+
+    const float mag_off_x = 5.97;
+    const float mag_off_y = 41.04;
+    const float mag_off_z = -5.65;
+
+    const float mag_slope_x = 1.19;
+    const float mag_slope_y = 0.87;
+    const float mag_slope_z = 0.99;
+
+    const int filter_frequency = 15;
+
+    uint32_t last_update_time_ms = 0;
+    const uint32_t update_interval_ms = 50;
 };
 
 struct Context
@@ -96,9 +121,67 @@ void update(USContext &ctx)
     Serial.write(buf);
 }
 
-void init(IMUContext &) {}
+void init(IMUContext &ctx)
+{
+    if (!IMU.begin())
+    {
+        Serial.println("Failed to initialize IMU!");
+        while (1)
+            ;
+    }
 
-void update(IMUContext &) {}
+    IMU.setMagnetOffset(ctx.mag_off_x, ctx.mag_off_y, ctx.mag_off_z);
+    IMU.setMagnetSlope(ctx.mag_slope_x, ctx.mag_slope_y, ctx.mag_slope_z);
+
+    ctx.filter.begin(ctx.filter_frequency);
+}
+
+void update(IMUContext &ctx)
+{
+    if (!IMU.accelerationAvailable() ||
+        !IMU.gyroscopeAvailable() ||
+        !IMU.magneticFieldAvailable())
+    {
+        return;
+    }
+
+    const auto current_time_ms = millis();
+    if (current_time_ms < ctx.last_update_time_ms + ctx.update_interval_ms)
+    {
+        return;
+    }
+
+    ctx.last_update_time_ms = current_time_ms;
+
+    float ax, ay, az;
+    float gx, gy, gz;
+    float mx, my, mz;
+    float roll, pitch, yaw;
+
+    IMU.readAcceleration(ax, ay, az);
+    IMU.readGyroscope(gx, gy, gz);
+    IMU.readMagneticField(mx, my, mz);
+
+    ax += ctx.offset_ax;
+    ay += ctx.offset_ay;
+    az += ctx.offset_az;
+
+    gx += ctx.offset_gx;
+    gy += ctx.offset_gy;
+    gz += ctx.offset_gz;
+
+    ctx.filter.update(gx, gy, gz, ax, ay, az, mx, my, mz);
+
+    yaw = ctx.filter.getYaw();
+    pitch = ctx.filter.getPitch();
+    roll = ctx.filter.getRoll();
+
+    // Loop delay to control data rate
+    char buf[32];
+
+    snprintf(buf, sizeof(buf), "imu %.2f %.2f %.2f\n", yaw, pitch, roll);
+    Serial.write(buf);
+}
 
 void init(Context &ctx)
 {
