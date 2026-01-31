78826ab2f62c0fab85ae58006867af84683ddaa3
Dima Kochetov <dima.kochetov.70@gmail.com>
2026-01-31 09:41:07 +0000

    Rewrite settings explicitly

--- src/teensy/main.cpp
+++ src/teensy/main.cpp
@@ -11,18 +11,6 @@
 #include "utils/io.h"
 #include "utils/time.h"
 
-constexpr auto left_pwm_fwd = 1531;
-constexpr auto left_pwm_bwd = 1484;
-constexpr auto left_pwm_stop = left_pwm_bwd + (left_pwm_fwd - left_pwm_bwd) / 2;
-constexpr auto left_pwm_deadband_fwd = left_pwm_fwd - left_pwm_stop;
-constexpr auto left_pwm_deadband_bwd = left_pwm_stop - left_pwm_bwd;
-
-constexpr auto right_pwm_fwd = 1530;
-constexpr auto right_pwm_bwd = 1486;
-constexpr auto right_pwm_stop = right_pwm_bwd + (right_pwm_fwd - right_pwm_bwd) / 2;
-constexpr auto right_pwm_deadband_fwd = right_pwm_fwd - right_pwm_stop;
-constexpr auto right_pwm_deadband_bwd = right_pwm_stop - right_pwm_bwd;
-
 struct Config {
   utils::io::Settings io_setings{
       .serial_baud = 115200,
@@ -41,9 +29,9 @@ struct Config {
                       .feedback_pin = Pin{6},
                       .feedback_pwm_duty_cycle_min = 0.0288f,
                       .feedback_pwm_duty_cycle_max = 0.9712f,
-                      .pwm_stop = Pwm{left_pwm_stop},
-                      .pwm_deadband_fwd = Pwm{left_pwm_deadband_fwd},
-                      .pwm_deadband_bwd = Pwm{left_pwm_deadband_bwd},
+                      .pwm_stop = Pwm{1507},
+                      .pwm_deadband_fwd = Pwm{24},
+                      .pwm_deadband_bwd = Pwm{23},
                   },
           },
       .right =
@@ -55,9 +43,9 @@ struct Config {
                       .feedback_pin = Pin{8},
                       .feedback_pwm_duty_cycle_min = 0.0286f,
                       .feedback_pwm_duty_cycle_max = 0.9714f,
-                      .pwm_stop = Pwm{right_pwm_stop},
-                      .pwm_deadband_fwd = Pwm{right_pwm_deadband_fwd},
-                      .pwm_deadband_bwd = Pwm{right_pwm_deadband_bwd},
+                      .pwm_stop = Pwm{1508},
+                      .pwm_deadband_fwd = Pwm{22},
+                      .pwm_deadband_bwd = Pwm{22},
                   },
           },
   };
