04a7e763f870129a4e2205bcd486da2e7e5ceaa0
Dima Kochetov <dima.kochetov.70@gmail.com>
2026-01-31 19:54:11 +0000

    Remove PID controller

--- src/teensy/utils/control.cpp
+++ src/teensy/utils/control.cpp
@@ -8,14 +8,6 @@
 namespace utils::control {
 namespace {
 
-PIDParams to_params(const PIDSettings &settings) {
-  return {
-      .K_p = settings.G,
-      .K_i = settings.T_i > 0.0f ? settings.G / settings.T_i : 0.0f,
-      .K_d = settings.G * settings.T_d,
-  };
-}
-
 float get_trajectory_value(const Trajectory &trajectory, const float t) {
   if (t <= trajectory[0].time) {
     return trajectory[0].value;
@@ -31,58 +23,6 @@ float get_trajectory_value(const Trajectory &trajectory, const float t) {
   return trajectory.back().value;
 }
 } // namespace
-
-PID::PID(const PIDSettings &settings) : settings_(settings), params_(to_params(settings)) {}
-
-float PID::update(const float setpoint, const float measurement, const float dt) {
-  const auto error = setpoint - measurement;
-  const auto prev_error = std::exchange(prev_error_, error);
-
-  const auto P = params_.K_p * error;
-  const auto I = params_.K_i * integrator_;
-
-  const auto D = [&] {
-    if (std::exchange(first_run_, false)) {
-      return 0.0f;
-    }
-    return params_.K_d * (error - prev_error) / dt;
-  }();
-
-  const auto output = P + I + D;
-  const auto pushing_saturation =
-      (output > settings_.out_max && error > 0) || (output < settings_.out_min && error < 0);
-
-  if (!pushing_saturation) {
-    integrator_ += error * dt;
-  }
-
-  if (std::abs(output) < settings_.deadband) {
-    return 0.0f;
-  }
-
-  return std::clamp(output, settings_.out_min, settings_.out_max);
-}
-
-void PID::configure(std::string_view setting, float value) {
-  if (setting == "g") {
-    settings_.G = value;
-  } else if (setting == "t-i") {
-    settings_.T_i = value;
-    integrator_ = 0;
-  } else if (setting == "t-d") {
-    settings_.T_d = value;
-  } else if (setting == "out-min") {
-    settings_.out_min = value;
-  } else if (setting == "out-max") {
-    settings_.out_max = value;
-  } else if (setting == "deadband") {
-    settings_.deadband = value;
-  } else {
-    io::error("PID: unknown setting: %s", setting.data());
-  }
-
-  params_ = to_params(settings_);
-}
 
 Ramp::Ramp(float rise_rate, float fall_rate) : rise_rate_(rise_rate), fall_rate_(fall_rate) {}
 
--- src/teensy/utils/control.h
+++ src/teensy/utils/control.h
@@ -7,39 +7,6 @@
 
 namespace utils::control {
 
-struct PIDSettings {
-  float G;
-  float T_i;
-  float T_d;
-  float out_min;
-  float out_max;
-  float deadband;
-};
-
-struct PIDParams {
-  float K_p;
-  float K_i;
-  float K_d;
-};
-
-class PID {
-public:
-  explicit PID(const PIDSettings &settings);
-
-public:
-  float update(float setpoint, float measurement, float dt);
-
-  void configure(std::string_view setting, float value);
-
-private:
-  PIDSettings settings_;
-  PIDParams params_;
-  float integrator_;
-  float prev_error_;
-  bool first_run_ = true;
-  bool log_ = true;
-};
-
 class Ramp {
 public:
   Ramp(float rise_rate, float fall_rate);
@@ -53,7 +20,7 @@ public:
 private:
   float rise_rate_; // units per second
   float fall_rate_; // units per second
-  float value_ = 0.0f;
+  float value_{0.0f};
 };
 
 struct TrajectoryPoint {
